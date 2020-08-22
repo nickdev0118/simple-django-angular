80a422615bbe84f39bb7d85000d99cc7861b9980
Sukhy <sukhindermann@gmail.com>
2020-08-22 14:21:32 +0000

    🎉 Initial Commit

---
+++ django_x_ng/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_x_ng.users.models import User
+from django_x_ng.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
